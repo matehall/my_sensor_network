1492faad0c437f1f38490c39be369134120a3663
Markus Tenghall <matehall@gmail.com>
2014-08-07 00:53:31 +0200

    Not working yet.

--- Pool_sensor/Pool_sensor.cpp
+++ Pool_sensor/Pool_sensor.cpp
@@ -2,47 +2,20 @@
 #include "Pool_sensor.h"
 
 void setup() {
-	Serial.println("DHT-11 sensor starting up");
-	dht.setup(HUMIDITY_SENSOR_DIGITAL_PIN);
+	Serial.println("Pool sensor starting up");
+
+	sensors.begin();
 	gw.begin(my_radio_id);
 //
 	// Send the Sketch Version Information to the Gateway
-	gw.sendSketchInfo("DHT-11", "1.0");
+	gw.sendSketchInfo("Pool", "1.0");
+	numSensors = sensors.getDeviceCount();
 
-//	// Register all sensors to gw (they will be created as child devices)
-	gw.sendSensorPresentation(CHILD_ID_HUM, S_HUM);
-	gw.sendSensorPresentation(CHILD_ID_TEMP, S_TEMP);
-//
-//	metric = gw.isMetricSystem();
-
-
-}
-
-void take_temp_reading() {
-	float temperature = dht.getTemperature();
-	if (isnan(temperature)) {
-		Serial.println("Failed reading temperature from DHT");
-		Serial.println(temperature);
-
-	} else if (temperature != lastTemp) {
-		lastTemp = temperature;
-
-		gw.sendVariable(lcd_radio_id, CHILD_ID_TEMP, V_TEMP, temperature, 1);
-		Serial.print("T: ");
-		Serial.println(temperature);
+	// Register all sensors to gw (they will be created as child devices)
+	for (int i = 0; i < numSensors && i < MAX_ATTACHED_DS18B20; i++) {
+		gw.sendSensorPresentation(i, S_TEMP);
 	}
-}
-
-void take_humidity_reading() {
-	float humidity = dht.getHumidity();
-	if (isnan(humidity)) {
-		Serial.println("Failed reading humidity from DHT");
-	} else if (humidity != lastHum) {
-		lastHum = humidity;
-		gw.sendVariable(lcd_radio_id, CHILD_ID_HUM, V_HUM, humidity, 1);
-		Serial.print("H: ");
-		Serial.println(humidity);
-	}
+	metric = gw.isMetricSystem();
 }
 
 void power_down() {
@@ -57,11 +30,28 @@ void power_down() {
 }
 
 void loop() {
-	Serial.println("reading");
-	delay(dht.getMinimumSamplingPeriod());
 
-	take_temp_reading();
-	take_humidity_reading();
-
-	//power_down(); // not working
+	sensors.requestTemperatures(); // Fetch temperatures from Dallas
+	delay(100);
+	for (int i = 0; i < numSensors && i < MAX_ATTACHED_DS18B20; i++) {
+		// Fetch and round temperature to one decimal
+		float temperature =
+				static_cast<float>(static_cast<int>((
+						metric ?
+								sensors.getTempCByIndex(i) :
+								sensors.getTempFByIndex(i)) * 10.)) / 10.;
+		// Only send data if temperature has changed and no error
+		if (lastTemperature[i] != temperature && temperature != -127.00) {
+			gw.powerUp(); // Powerup introduces a small delay (which is missing in radio.write powerup)
+			// Send variable (using registered shortcut) to gw
+			gw.sendVariable(i, V_TEMP, temperature, 1);
+			lastTemperature[i] = temperature;
+		}
+	}
+	// Power down the radio.  Note that the radio will get powered back up
+	// on the next write() call.
+	delay(500);
+	gw.powerDown();
+	sleep.pwrDownMode(); //set sleep mode
+	sleep.sleepDelay(SLEEP_TIME * 1000); //sleep for: sleepTime
 }
--- Pool_sensor/Pool_sensor.h
+++ Pool_sensor/Pool_sensor.h
@@ -1,33 +1,32 @@
-// Only modify this file to include
-// - function definitions (prototypes)
-// - include files
-// - extern variable definitions
-// In the appropriate section
 
 #ifndef _Pool_sensor_H_
 #define _Pool_sensor_H_
+
 #include "Arduino.h"
 //add your includes for the project Pool_sensor here
 #include <Sleep_n0m1.h>
 #include <SPI.h>
 #include <EEPROM.h>
-#include <DallasTemperature.h>
 #include <OneWire.h>
+#include <DallasTemperature.h>
 #include <RF24.h>
 #include <Sensor.h>
 
-#define ONE_WIRE_BUS 3 // Pin where dallase sensor is connected
+#define ONE_WIRE_BUS 3
 #define MAX_ATTACHED_DS18B20 16
 
-unsigned long SLEEP_TIME = 60; // Sleep time between reads (in seconds)
+unsigned long SLEEP_TIME = 60;
 
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
 Sensor gw;
-DHT dht;
 Sleep sleep;
-float lastTemp;
-float lastHum;
+
+float lastTemperature[MAX_ATTACHED_DS18B20];
+int numSensors = 0;
 boolean metric = true;
-uint8_t my_radio_id = 23;
+
+uint8_t my_radio_id = 24;
 uint8_t lcd_radio_id = 10;
 
 //end of add your includes here
